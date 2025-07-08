000076891bd07d243276be03839bb3555293cdff
Joshua Irmer <joshua.irmer@gmail.com>
2025-07-08 21:11:19 +0200

    feat(install): improve install script

--- install.sh
+++ install.sh
@@ -1,21 +1,65 @@
 #!/usr/bin/env bash
 
+hypr_dir=${HOME}/.config/hypr
+waybar_dir=${HOME}/.config/waybar
+
+clean() {
+    echo "deleting hypr dir..."
+    rm -rf ${hypr_dir}
+    
+    echo "deleting waybar dir..."
+    rm -rf ${waybar_dir}
+}
+
 yay_packages() {
     yay -Syu walker-bin google-chrome
 }
 
 conf_dir() {
     echo "creating hyper dir..."
-    mkdir ${HOME}/.config/hypr
+    mkdir -p ${hypr_dir}
+    
+    echo "creating waybar dir..."
+    mkdir -p ${waybar_dir}
 }
 
 links() {
-    ln -s ${HOME}/.config/hypr hypr/hyprland.conf
+    echo "link hypr config..."
+    ln -s $(pwd)/hypr/* ${hypr_dir}
+
+    echo "link waybar config..."
+    ln -s $(pwd)/waybar/* ${waybar_dir}
 }
 
-main() {
-    # yay_packages
-    conf_dir
+reload() {
+    echo "reload hypr..."
+    hyprctl reload
+    
+    echo "reload waybar..." 
+    pkill waybar && hyprctl dispatch exec waybar
 }
 
-main
+while getopts 'cdlryh' opt; do
+    case "$opt" in
+    c)
+        clean
+        ;;
+    d)
+        conf_dir
+        ;;
+    l)
+        links
+        ;;
+    y)
+        yay_packages
+        ;;
+    r)
+        reload
+        ;;
+    ? | h)
+        echo "Usage: $(basename $0) [-c] [-d] [-l] [-r] [-y]"
+        exit 1
+        ;;
+    esac
+done
+shift "$(($OPTIND - 1))"
